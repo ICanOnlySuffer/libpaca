c5e5c8218de056cedb61fb4a9c140bcae4b9a4ec
ICanOnlySuffer <pierorojas@tutamail.com>
2022-05-19 22:49:24 -0500

    cleaned up

--- inc/drawable/rectangle.h
+++ inc/drawable/rectangle.h
@@ -3,13 +3,14 @@
 # define PGE_DRAWABLE_RECTANGLE_H
 # include "../drawable.h"
 
-ext nil rectangle_draw (drawable_t * rectangle);
+ext nil rectangle_draw_lines (drawable_t * rectangle);
+ext nil rectangle_draw_full (drawable_t * rectangle);
 ext nil rectangle_free (ptr * data);
-ext drawable_t * rectangle_new_ (color_t * color, rect_t rect);
+ext drawable_t * rectangle_new_ (color_t * color, frect_t rect);
 # define rectangle_new(color_, ...) \
 	rectangle_new_ ( \
 		color_, \
-		(SDL_Rect) { \
+		(frect_t) { \
 			.x = 0, \
 			.y = 0, \
 			.w = 1, \
--- src/drawable/rectangle.c
+++ src/drawable/rectangle.c
@@ -1,42 +1,24 @@
 # include "../../inc/drawable/rectangle.h"
 
-nil rectangle_draw (drawable_t * rectangle) {
-	color_t current_color;
-	SDL_GetRenderDrawColor (
-		WINDOW.renderer,
-		&current_color.r,
-		&current_color.g,
-		&current_color.b,
-		&current_color.a
-	);
-	
-	color_t * color = (color_t *) rectangle -> data [0];
-	SDL_SetRenderDrawColor (
-		WINDOW.renderer,
-		color -> r,
-		color -> g,
-		color -> b,
-		color -> a
-	);
-	SDL_RenderDrawRect (WINDOW.renderer, &rectangle -> rect);
-	SDL_SetRenderDrawColor (
-		WINDOW.renderer,
-		current_color.r,
-		current_color.g,
-		current_color.b,
-		current_color.a
-	);
+nil rectangle_draw_lines (drawable_t * rectangle) {
+	render_set_draw_color ((color_t *) rectangle -> data [0]);
+	render_draw_rect_lines (&rectangle -> rect);
+}
+
+nil rectangle_draw_full (drawable_t * rectangle) {
+	render_set_draw_color ((color_t *) rectangle -> data [0]);
+	render_draw_rect_full (&rectangle -> rect);
 }
 
 nil rectangle_free (ptr * data) {
 	// nothing to free
 }
 
-drawable_t * rectangle_new_ (color_t * color, rect_t rect) {
+drawable_t * rectangle_new_ (color_t * color, frect_t rect) {
 	drawable_t * rectangle = malloc (sizeof (drawable_t));
-	rectangle -> data = malloc (sizeof (ptr));
+	rectangle -> data = malloc (sizeof (ptr [2]));
 	rectangle -> data [0] = color;
-	rectangle -> draw = rectangle_draw;
+	rectangle -> draw = rectangle_draw_full;
 	rectangle -> free = rectangle_free;
 	rectangle -> rect.w = rect.w;
 	rectangle -> rect.h = rect.h;
