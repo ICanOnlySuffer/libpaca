4ad9dbd6162723ddf9ca977215c5ee072fbe58dd
ICanOnlySuffer <pierorojas@tutamail.com>
2022-07-05 13:04:22 -0500

    renamed procs

--- inc/vectors.h
+++ inc/vectors.h
@@ -18,16 +18,16 @@ typedef struct {
 ext vector_t vector_new (str name, u16 size);
 ext nil vector_free (vector_t * vector);
 
-ext u08 vector_psh (vector_t * vector, ptr item);
-ext u08 vector_rmv (vector_t * vector, ptr item);
+ext u08 vector_push (vector_t * vector, ptr item);
+ext u08 vector_remove (vector_t * vector, ptr item);
 
-ext u08 on_update_psh (proc_t * proc);
-ext u08 on_event_psh (proc_t * proc);
-ext u08 at_quit_psh (proc_t * proc);
+ext u08 on_update_push (proc_t * proc);
+ext u08 on_event_push (proc_t * proc);
+ext u08 at_quit_push (proc_t * proc);
 
-ext u08 on_update_rmv (proc_t * proc);
-ext u08 on_event_rmv (proc_t * proc);
-ext u08 at_quit_rmv (proc_t * proc);
+ext u08 on_update_remove (proc_t * proc);
+ext u08 on_event_remove (proc_t * proc);
+ext u08 at_quit_remove (proc_t * proc);
 
 ext nil vectors_on_update ();
 ext nil vectors_on_event (event_t * event);
--- src/vectors.c
+++ src/vectors.c
@@ -1,12 +1,12 @@
 # include "../inc/vectors.h"
 # include "../inc/log.h"
 
-static vector_t ON_UPDATE;
-static vector_t ON_EVENT;
-static vector_t AT_QUIT;
+prv vector_t ON_UPDATE;
+prv vector_t ON_EVENT;
+prv vector_t AT_QUIT;
 
 vector_t vector_new (str name, u16 capacity) {
-	LOG ("%s = Vector.new %u", (u64) name, (u64) capacity);
+	LOG ("%s = Vector.new %u", name, (u64) capacity);
 	ret (vector_t) {
 		.items = malloc (sizeof (ptr) * capacity),
 		.capacity = capacity,
@@ -16,47 +16,47 @@ vector_t vector_new (str name, u16 capacity) {
 }
 
 nil vector_free (vector_t * vector) {
-	LOG ("%s.items.free", (u64) vector -> name);
+	LOG ("%s.items.free", vector -> name);
 	free (vector -> items);
 }
 
-u08 vector_psh (vector_t * vector, ptr item) {
-	LOG ("%s.psh %x", (u64) vector -> name, (u64) item);
-	if (vec_psh ((vec *) vector, item)) {
+u08 vector_push (vector_t * vector, ptr item) {
+	LOG ("%s.push %x", vector -> name, item);
+	if (vec_push ((vec *) vector, item)) {
 		ret true;
 	}
-	LOG_ERR ("not %s.psh %x", (u64) vector -> name, (u64) item);
+	ERR ("not %s.push %x", vector -> name, item);
 	ret false;
 }
 
-u08 vector_rmv (vector_t * vector, ptr item) {
-	LOG ("%s.rmv %x", (u64) vector -> name, (u64) item);
-	if (vec_inc ((vec *) vector, item)) {
-		vec_rmv ((vec *) vector, item);
+u08 vector_remove (vector_t * vector, ptr item) {
+	LOG ("%s.remove %x", vector -> name, item);
+	if (vec_includes ((vec *) vector, item)) {
+		vec_remove ((vec *) vector, item);
 		ret true;
 	}
-	LOG_ERR ("not %s.inc %x", (u64) vector -> name, (u64) item);
+	ERR ("not %s.includes %x", vector -> name, item);
 	ret false;
 }
 
-u08 on_update_psh (proc_t * proc) {
-	ret vector_psh (&ON_UPDATE, (ptr) proc);
+u08 on_update_push (proc_t * proc) {
+	ret vector_push (&ON_UPDATE, (ptr) proc);
 }
-u08 on_event_psh (proc_t * proc) {
-	ret vector_psh (&ON_EVENT, (ptr) proc);
+u08 on_event_push (proc_t * proc) {
+	ret vector_push (&ON_EVENT, (ptr) proc);
 }
-u08 at_quit_psh (proc_t * proc) {
-	ret vector_psh (&AT_QUIT, (ptr) proc);
+u08 at_quit_push (proc_t * proc) {
+	ret vector_push (&AT_QUIT, (ptr) proc);
 }
 
-u08 on_update_rmv (proc_t * proc) {
-	ret vector_rmv (&ON_UPDATE, (ptr) proc);
+u08 on_update_remove (proc_t * proc) {
+	ret vector_remove (&ON_UPDATE, (ptr) proc);
 }
-u08 on_event_rmv (proc_t * proc) {
-	ret vector_rmv (&ON_EVENT, (ptr) proc);
+u08 on_event_remove (proc_t * proc) {
+	ret vector_remove (&ON_EVENT, (ptr) proc);
 }
-u08 at_quit_rmv (proc_t * proc) {
-	ret vector_rmv (&AT_QUIT, (ptr) proc);
+u08 at_quit_remove (proc_t * proc) {
+	ret vector_remove (&AT_QUIT, (ptr) proc);
 }
 
 nil vectors_on_update () {
@@ -75,7 +75,7 @@ nil vectors_on_event (event_t * event) {
 	}
 }
 
-static u08 _vectors_at_quit () {
+prv u08 _vectors_at_quit () {
 	for (u08 i = AT_QUIT.size - 1; i; i--) {
 		((u08 (*) ()) ((proc_t *) AT_QUIT.items [i]) -> proc) ();
 	}
@@ -83,19 +83,19 @@ static u08 _vectors_at_quit () {
 }
 proc_t vectors_at_quit = {"Vectors.at_quit", _vectors_at_quit};
 
-static u08 _vectors_quit () {
+prv u08 _vectors_quit () {
 	vector_free (&ON_UPDATE);
 	vector_free (&ON_EVENT);
 	vector_free (&AT_QUIT);
 	ret true;
 }
-static proc_t vectors_quit = {"Vectors.quit", _vectors_quit};
+prv proc_t vectors_quit = {"Vectors.quit", _vectors_quit};
 
-static u08 _vectors_init () {
+prv u08 _vectors_init () {
 	ON_UPDATE = vector_new ("ON_UPDATE", 4);
 	ON_EVENT = vector_new ("ON_EVENT", 4);
 	AT_QUIT = vector_new ("AT_QUIT", 4);
-	at_quit_psh (&vectors_quit);
+	at_quit_push (&vectors_quit);
 	ret true;
 }
 proc_t vectors_init = {"Vectors.init", _vectors_init};
