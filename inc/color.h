fbca0b9ff6db9fdcb194ca59bdba7143558ecb94
ICanOnlySuffer <pierorojas@tutamail.com>
2022-07-09 11:52:13 -0500

    added default colors

---
+++ inc/color.h
@@ -0,0 +1,11 @@
+
+# ifndef LIBPACA_COLOR_H
+# define LIBPACA_COLOR_H
+# include <SDL2/SDL_pixels.h>
+# include <pocha/cor.h>
+
+typedef SDL_Color color_t;
+ext color_t * COLORS [];
+
+# endif // LIBPACA_COLOR_H
+
